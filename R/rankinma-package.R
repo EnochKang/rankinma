e045bdf393dbed1524af57664330d567452cae25
EnochKang <y.enoch.kang@gmail.com>
2023-02-19 13:26:09 +0800

    Create package *rankinma*
    The first release of package *rankinma*.

---
+++ R/rankinma-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom graphics axis
+#' @importFrom graphics barplot
+#' @importFrom graphics lines
+#' @importFrom graphics mtext
+#' @importFrom graphics par
+#' @importFrom graphics points
+#' @importFrom graphics segments
+#' @importFrom graphics text
+#' @importFrom graphics title
+#' @importFrom grDevices recordPlot
+#' @importFrom grDevices rgb
+#' @importFrom stats aggregate
+## usethis namespace: end
+NULL
